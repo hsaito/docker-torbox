99b2bc69ac6f60bf3a22ca9be7901173d2b6f6bd
Hideki Saito <hideki@hidekisaito.com>
2014-03-21 17:04:32 -0700

    Changed the torbox-hidden webserver to lighttpd. Added the script to attempt to generate hidden service address by briefly running tor for a second.

--- torbox-hidden/files/startup.sh
+++ torbox-hidden/files/startup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-service apache2 start
+tor --verify-config
+service lighttpd start
 service ssh start
-echo "Your hostname:"
-cat var/lib/tor/hidden_service/hostname
+if [ -f /var/lib/tor/hidden_service/hostname ]; then
+	echo "Your hostname:"
+	cat /var/lib/tor/hidden_service/hostname
+else
+	tor & sleep 1; killall tor ; echo "Your hostname:" ; cat /var/lib/tor/hidden_service/hostname
+fi
+
 tor
