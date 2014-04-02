9ba66e1c072c4c75bb0295cf3eaa0059b130277c
Hideki Saito <hideki@hidekisaito.com>
2014-04-01 22:10:35 -0700

    Increased initial wait time to 10 second to give it enough time to get hostname.

--- torbox-hidden/files/startup.sh
+++ torbox-hidden/files/startup.sh
@@ -5,7 +5,8 @@ if [ -f /var/lib/tor/hidden_service/hostname ]; then
 	echo "Your hostname:"
 	cat /var/lib/tor/hidden_service/hostname
 else
-	service tor start & sleep 1; service tor stop ; echo "Your hostname:" ; cat /var/lib/tor/hidden_service/hostname
+	echo "Starting up Tor briefly to generate your hidden service hostname. Please wait..."
+	service tor start & sleep 10; service tor stop ; echo "Your hostname:" ; cat /var/lib/tor/hidden_service/hostname
 	echo "Regenerating server keys..."
 	chmod +x /sshd-key-gen.sh ; /sshd-key-gen.sh
 	rm /sshd-key-gen.sh
