edf327f39bea2afb79069fb42a3731983312c437
Hideki Saito <hideki@hidekisaito.com>
2014-05-01 15:15:11 -0700

    Changed base hsaito/torbox image to not use cache

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd torbox ; docker build -t hsaito/torbox . ; cd ..
+cd torbox ; docker build --no-cache=true -t hsaito/torbox . ; cd ..
 cd torbox-relay ; docker build -t hsaito/torbox-relay . ; cd ..
 cd torbox-hidden ; docker build -t hsaito/torbox-hidden . ; cd ..
