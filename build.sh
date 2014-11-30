b1e5f6700785318c8256ecf402cdf42db9c737b7
Hideki Saito <hideki@hidekisaito.com>
2014-11-30 02:21:04 -0800

    Modified build.sh to take command argument for username

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-cd torbox ; docker build --no-cache=true -t hsaito/torbox . ; cd ..
-cd torbox-relay ; docker build -t hsaito/torbox-relay . ; cd ..
-cd torbox-hidden ; docker build -t hsaito/torbox-hidden . ; cd ..
+
+if [[ $1 == "" ]] ; then
+        echo "Needs username"
+        exit 2
+fi
+
+cd torbox ; docker build --no-cache=true -t $1/torbox . ; cd ..
+cd torbox-relay ; docker build -t $1/torbox-relay . ; cd ..
+cd torbox-hidden ; docker build -t $1/torbox-hidden . ; cd ..
